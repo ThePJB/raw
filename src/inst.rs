cf8a26d8481d7ed2d855e1328ac75f96fd43ea76
Patrick Bell <zclpatrick@gmail.com>
2024-06-18 20:23:23 +1000

    instrument working

--- src/inst.rs
+++ src/inst.rs
@@ -7,18 +7,38 @@ pub const FS: f64 = 48000.0;
 pub const TS: f64 = 1.0 / FS;
 
 pub struct Instrument {
+    reverb: ReverbChain,
     rc: RenderContext,
     sc: SoundContext,
     n: i64,
     t: f64,
+    harm: f32,
+    mag: f32,
+    rng: u32,
+
+    click_point: Vec2,
 }
 impl Instrument {
     pub fn new(gl: glow::Context) -> Self {
         let mut inst = Instrument {
             rc: RenderContext::new(gl),
             sc: SoundContext::new(None, vec![]),
+            reverb: ReverbChain::new(vec![
+                // vec2(0.999, 0.01),
+                // vec2(0.999, 0.02),
+                vec2(0.99999, 0.005),
+                vec2(0.999, 0.01),
+                vec2(0.999, 0.02),
+                vec2(0.99, 0.23),
+                vec2(0.99, 0.22),
+                vec2(0.99, 0.44),
+            ]),
             n: 0,
             t: 0.0,
+            rng: random_seed(),
+            click_point: vec2(0.5, 0.5),
+            harm: 1.0,
+            mag: 0.0,
         };
         inst.initialize();
         inst
@@ -26,34 +46,51 @@ impl Instrument {
     pub fn initialize(&mut self) {
         self.sc.send_samples(repeat(0.0).take(4000));
     }
+    // x and y real stereo sample
+    pub fn sample(&mut self) -> Vec2 {
+        self.n += 1;
+        self.t += TS;
+        // let z = self.reverb.tick(vec2(self.mag, 0.0), self.harm); // maybe this is a good parameter to make random
+        let z = self.reverb.tick(vec2(self.mag, self.harm), 1.0); // maybe this is a good parameter to make random
+        let z = squash(z);
+        let z = z.pol2cart();
+        let stereo = vec2(z.x,z.y);
+        stereo*0.1
+    }
 }
 impl App for Instrument {
     fn frame(&mut self, input: Input) {
         /*
          * Update states
          */
+        let mouse = input.mouse_px/self.rc.wh.as_vec2();
 
+        // hang on it was meant to be the pitch of the first oscillator too
+        // oh yeah a first reverb not really or a first reverb state
 
+        // let click = if input.click_lmb { 1.0 } else { 0.0 };
+        if input.held_lmb {
+            self.click_point = mouse;
+            self.harm = mouse.x*TAU;
+            self.mag = self.click_point.y;
+        } else {
+            self.mag *= 0.99;
+            // self.mag = 0.0;
+        }
         /*
          * Send Audio
          */
         let e_sec = input.elapsed_time.as_secs_f64();
         let e_samp = (e_sec * FS) as i64;
-        // sample of t aye
-        let samples = (0..(2*e_samp))
-            .map(|n| n/2) // channels
-            .map(|n| self.t + n as f64 * TS)    // sample number to time
-            .map(|t| (440.0*TAU*t as f32).sin())   // time to amplitude
-            .map(|x| x * 0.01); // volume
-        self.sc.send_samples(samples);
-        self.n += e_samp;
-        self.t += e_sec;
 
-        /*
-         * Draw UI
-         */
+        let mut buf = Vec::new();
 
-        
+        for i in 0..e_samp {
+            let w = self.sample();
+            buf.push(w.x);
+            buf.push(w.y);
+        }
+        self.sc.send_samples(buf.into_iter());
 
     }
 }
--- src/reverb.rs
+++ src/reverb.rs
@@ -6,76 +6,49 @@ pub fn squash(z: Vec2) -> Vec2 {
 
 pub struct Reverb {
     mem: Vec2,
-    wr: Vec2,
-    wt: Vec2,
+    w: Vec2,
 }
 impl Reverb {
     pub fn new(w: Vec2) -> Self {
         Self {
             mem: vec2(0.0, 0.0),
-            wr: w,
-            wt: w,
+            w,
         }
     }
-    pub fn new2(wr: Vec2, wt: Vec2) -> Self {
-        Self {
-            mem: vec2(0.0, 0.0),
-            wr,
-            wt,
-        }
-    }
-    pub fn tick(&mut self, z: Vec2) -> Vec2 {
-        let prev = self.mem.cmul_pol(self.wr);
+    pub fn tick(&mut self, z: Vec2, harm: f32) -> Vec2 {
+        let mut w = self.w;
+        w.y *= harm;
+        let prev = self.mem.cmul_pol(w);
         let zcart = prev.pol2cart() + z.pol2cart();
         let zpol = zcart.cart2pol();
         self.mem = zpol;
-        // assert!(zpol.x < 2000.0);
-        return zpol.cmul_pol(self.wt);
+        return zpol.cmul_pol(w);
     }
 }
 pub struct ReverbChain {
-    reverbs: Vec<Reverb>,
+    pub reverbs: Vec<Reverb>,
+}
+impl Default for ReverbChain {
+    fn default() -> Self {
+        Self {
+            reverbs: vec![],
+        }
+    }
 }
 impl ReverbChain {
     pub fn new(w: Vec<Vec2>) -> Self {
         Self {
             reverbs: w.into_iter().map(|w| Reverb::new(w)).collect(),
+            ..Default::default()
         }
     }
-    pub fn new2(w: Vec<(Vec2, Vec2)>) -> Self {
-        Self {
-            reverbs: w.into_iter().map(|w| Reverb::new2(w.0, w.1)).collect(),
-        }
-    }
-    pub fn tick(&mut self, mut z: Vec2) -> Vec2 {
+    pub fn tick(&mut self, mut z: Vec2, harm: f32) -> Vec2 {
         for reverb in self.reverbs.iter_mut() {
-            z = reverb.tick(z);
-        }
-        z
-    }
-    pub fn tick_nonlinear(&mut self, mut z: Vec2) -> Vec2 {
-        for reverb in self.reverbs.iter_mut() {
-            z = reverb.tick(z);
+            z = reverb.tick(z, harm);
             z = vec2(1.0 - (-z.x).exp(), z.y);
         }
         z = vec2(1.0 - (-z.x).exp(), z.y);
         z
     }
-    pub fn tick_nonlinear_xc(&mut self, mut z: Vec2) -> Vec2 {
-        for reverb in self.reverbs.iter_mut() {
-            z = reverb.tick(z);
-            z = vec2(1.0 - (-z.x).exp(), z.y);
-        }
-        z = vec2(1.0 - (-z.x).exp(), z.y);
-        z
-    }
-    pub fn tick_parallel(&mut self, z: Vec2) -> Vec2 {
-        let mut acc = vec2(0.0, 0.0);
-        for reverb in self.reverbs.iter_mut() {
-            let z = reverb.tick(z);
-            // let z = squash(z);
-            acc += z;
-        }
-        acc
-    }
+
 }
